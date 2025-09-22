8d188769858c70666ff115b3903465cc2f563597
Michael Lindsay <michaelduffielindsay@gmail.com>
2025-09-22 18:45:33 +0000

    initial config

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs@{ self, nixpkgs, disko, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        inputs.disko.nixosModules.disko
+      ];
+    };
+  };
+}
