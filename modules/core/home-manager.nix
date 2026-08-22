e4fda76861fb7b746cbde515b143b6ce81919c7e
michaellindsay23 <michaelduffielindsay@gmail.com>
2026-08-22 00:26:30 -0400

    fixing conflicts

--- modules/core/home-manager.nix
+++ modules/core/home-manager.nix
@@ -16,7 +16,7 @@
       imports =[ ../home ];
       home.username = "${username}";
       home.homeDirectory = "/home/${username}";
-      home.stateVersion = "25.11";
+      home.stateVersion = "26.05";
       programs.home-manager.enable = true;
     };
   };
